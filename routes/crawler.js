10058fa1a29dac3a8c220f712da00c6a8a7c762b
Gagandeep Singh <gagandeep.singh.iitdelhi@gmail.com>
2016-11-08 04:12:50 +0530

    Recursively calling crawling on multiple items

--- routes/crawler.js
+++ routes/crawler.js
@@ -1,16 +1,40 @@
 var express = require('express');
 var router = express.Router();
 var crawlerFunction = require('../crawlers/indiaspend');
+var db = require('../db');
+
+var i=0;
+var errors = [];
+var success = [];
 
 router.get('/indiaspend', function(req, res, next) {
-    crawlerFunction('868004027010993').then(function(data) {
-        console.log(data);
-        res.send(data);
+
+
+    db.indiaSpendCrawler.findAll().then(function(allRecords) {
+        i = 0;
+        errors = [];
+        success = [];
+        crawlerHelper(allRecords);
+
     }).catch(function(e) {
-        res.json({
-            "error-msg" : e
-        });
+        console.log('ERROR Reading records');
     });
 });
 
 module.exports = router;
+
+function crawlerHelper(allRecords) {
+    console.log(i);
+    if(i >= allRecords.length)
+        return;
+
+    crawlerFunction(allRecords[i].imei).then(function(data) {
+        console.log(data);
+        success.push(data);
+        ++i;
+        crawlerHelper(allRecords);
+    }).catch(function(e) {
+        console.log(e);
+        errors.push(e);
+    });
+}
