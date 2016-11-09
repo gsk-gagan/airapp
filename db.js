a720fc34c8865ce23effd89a9ddfa397e4641133
Gagandeep S. Kaler <gagandeep.singh.iitdelhi@gmail.com>
2016-11-09 12:28:52 +0530

    Added postgres for production

--- db.js
+++ db.js
@@ -1,9 +1,19 @@
 var path = require('path');
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(undefined, undefined, undefined, {
-    'dialect' : 'sqlite',
-    'storage' : path.join(__dirname, 'data', 'air-app-database.sqlite')
-});
+var env = process.env.NODE_ENV || 'development';
+var sequelize;
+
+if(env == 'production') {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres'
+    });
+} else {
+    sequelize = new Sequelize(undefined, undefined, undefined, {
+        'dialect' : 'sqlite',
+        'storage' : path.join(__dirname, 'data', 'air-app-database.sqlite')
+    });
+}
+
 var db = {};
 
 //Table Definitions
